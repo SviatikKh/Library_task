ceb702cdc9e30637581269a371f28a7824f7cb01
SviatikKh <sviatoslavhr@gmail.com>
2021-01-23 00:01:38 +0200

    Created projects, created Author model

---
+++ library/author/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(blank=True, max_length=20)
+    surname = models.CharField(blank=True, max_length=20)
+
+    def __str__(self):
+        return self.surname
+
+    @staticmethod
+    def create(name, surname):
+        author = Author(name=name, surname=surname)
+        author.save()
+        return author
+
+    @staticmethod
+    def get_by_id(author_id):
+        user = Author.objects.get(id=author_id)
+        return user
+
+    @staticmethod
+    def delete_by_id(author_id):
+        author = Author.objects.get(id=author_id)
+        author.delete()
+        return True
+
+    def update(self, name=None, surname=None):
+        if name:
+            self.name = name
+        if surname:
+            self.surname = surname
+        from django.db import transaction
+        with transaction.atomic():
+            self.save()
+
