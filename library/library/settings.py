3f1fcde36abad0c4bf2573f86a59ea96537cf4f9
SviatikKh <sviatoslavhr@gmail.com>
2021-01-26 16:27:28 +0200

    Added Docker

--- library/library/settings.py
+++ library/library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bf8(y^jvh2t2g&)*phv-_1uk+oab-ef%9_v4q3*c-c)s$@+*2e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
